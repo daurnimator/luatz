72212b1e2c8784cf956862ef5ba63046fd9fba3d
daurnimator <quae@daurnimator.com>
2013-11-23 11:47:36 -0500

    luatz/init: Remove unnecessary 'get' prefix

--- luatz/init.lua
+++ luatz/init.lua
@@ -1,13 +1,13 @@
 local get_tz = require "luatz.tzcache".get_tz
 
-local function gettimein ( tz , now )
+local function time_in ( tz , now )
 	return get_tz ( tz ):localize ( now )
 end
 
 return {
 	get_tz    = get_tz ;
-	gettime   = require "luatz.gettime".gettime ;
-	gettimein = gettimein ;
+	time      = require "luatz.gettime".gettime ;
+	time_in   = time_in ;
 	parse     = require "luatz.parse" ;
 	timetable = require "luatz.timetable" ;
 }

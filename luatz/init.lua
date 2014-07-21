d848dd053f0f087b61fc8849f8ea214bf5971473
daurnimator <quae@daurnimator.com>
2014-07-20 19:38:58 -0900

    luatz/init: Refactor

--- luatz/init.lua
+++ luatz/init.lua
@@ -1,13 +1,17 @@
-local get_tz = require "luatz.tzcache".get_tz
-
-local function time_in ( tz , now )
-	return get_tz ( tz ):localize ( now )
-end
-
-return {
-	get_tz    = get_tz ;
-	time      = require "luatz.gettime".gettime ;
-	time_in   = time_in ;
-	parse     = require "luatz.parse" ;
+local _M = {
+	parse = require "luatz.parse" ;
 	timetable = require "luatz.timetable" ;
 }
+
+--- Top-level aliases for common functions
+
+_M.time = require "luatz.gettime".gettime
+_M.get_tz = require "luatz.tzcache".get_tz
+
+--- Handy functions
+
+_M.time_in = function ( tz , now )
+	return _M.get_tz ( tz ):localize ( now )
+end
+
+return _M
